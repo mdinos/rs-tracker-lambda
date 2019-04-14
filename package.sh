16862edff52c65c647829eb395456b25a8612198
mdinos <32902031+mdinos@users.noreply.github.com>
2019-04-14 16:12:59 +0100

    Improve package.sh
    Tried it on my macbook.

--- package.sh
+++ package.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Get pip requirements
 pipreqs --force ./
+
+# install pip requirements in pwd
 pip install -r requirements.txt --target .
+
+# zip up folder contents and place them in ../
 zip -r9 ../rs_tracker_lambda.zip .
 
+# clean up
 rm -rf */
-rm six.py
+rm six.py*
 rm requirements.txt
