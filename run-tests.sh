446f5eff8e7e45e25cf13ffecbaa421d552edc6c
mdinos <32902031+mdinos@users.noreply.github.com>
2019-08-07 21:23:23 +0100

    Add publishing optional step to run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -4,3 +4,18 @@ export username=woofythedog
 export bucket=fake-bucket
 
 pytest ./rs_tracker_lambda_tests.py
+
+if [[ $? -eq 0 ]]; then 
+  echo "SUCCESS"
+else
+  echo "FAILURE"
+  exit 1
+fi
+
+if [ "$1" = "publish" ]; then
+  echo "PUBLISHING"
+  PWD=$(pwd)
+  ${PWD}/package.sh
+else
+  echo "NOT PUBLISHING"
+fi
